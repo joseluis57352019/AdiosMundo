7dd17b31f9efbbad6f6614708ee7dbe12beaf62a
Sergio Moreno <sermo14.sm@gmail.com>
2019-10-03 19:31:54 +0200

    Mejorando al jositoluiso

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -1,5 +1,8 @@
 public class AdiosMundo {
   public static void main (String[] arqs) {
+
+	System.out.println("Modificado by serg");
+
       System.out.println("Adios Mundo!!!!!!!!!!!!!");
 	System.out.Println("Pero Volveré");
      }
