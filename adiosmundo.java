634fdccfe513534962a5dcd6a34c6c044a600f30
Jose Luis <joseluis57352019@gmail.com>
2019-10-03 18:13:05 +0200

    Experimentando con colores

---
+++ adiosmundo.java
@@ -0,0 +1,15 @@
+
+public class adiosmundo {
+	public static void main (String [] args){
+	String amarillo= "\033[33m";
+	String verde= "\033[32m";
+	
+		
+	System.out.println(amarillo+"Adios Mundo");
+	System.out.println(verde+"pero volveré...");
+	
+		
+			
+	}
+
+}
